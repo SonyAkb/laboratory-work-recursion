0a540789b95811d384b9d81600435e4e85edcfe3
SonyAkb <145361459+SonyAkb@users.noreply.github.com>
2024-01-31 19:30:55 +0500

    Факториал через цикл

--- ЛР Рекурсия.cpp
+++ ЛР Рекурсия.cpp
@@ -3,17 +3,26 @@
 
 using namespace std;
 
-unsigned int factorial(unsigned int n)
+unsigned int Factorial_Recursive(unsigned int n)
 {
     if (n == 0)
         return 1;
-    return n * factorial(n - 1);
+    return n * Factorial_Recursive(n - 1);
+}
+
+unsigned int Factorial_Direct(unsigned int n)
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res = res * i;
+    }
+    return res;
 }
 
 double Direct_Calculation(int n, double x) { // прямое вычисление значения функции
     double sum = 0;
     for (int i = 0; i < n; i++) {
-        sum += pow(x, 2 * i + 1) / factorial(2 * i + 1);
+        sum += pow(x, 2 * i + 1) / Factorial_Direct(2 * i + 1);
         cout << i << ' ' << sum<<endl;
     }
     return sum; 
@@ -25,7 +34,7 @@ double Recursive_Calculation(int n, double x) {
         return x; // базовый случай
     }
     else {
-        qwe = pow(x, 2 * n + 1) / factorial(2 * n + 1) + Recursive_Calculation(n - 1, x);
+        qwe = pow(x, 2 * n + 1) / Factorial_Recursive(2 * n + 1) + Recursive_Calculation(n - 1, x);
         cout << n << ' ' << qwe << endl;
         return qwe; // рекурсивный случай
     }
@@ -33,7 +42,7 @@ double Recursive_Calculation(int n, double x) {
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    int n = 5;
+    int n = 6;
     double x = 2;
 
     double result1 = Recursive_Calculation(n - 1, x);
