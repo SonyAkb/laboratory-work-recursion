89934bc82ab686c06ad839a93db0bc36fa71fab3
SonyAkb <145361459+SonyAkb@users.noreply.github.com>
2024-02-01 22:14:00 +0500

    Перемещение функций под основную функцию

--- ЛР Рекурсия.cpp
+++ ЛР Рекурсия.cpp
@@ -3,39 +3,10 @@
 #include <chrono>
 using namespace std;
 
-unsigned int Factorial_Recursive(unsigned int n){ //вычисление факториала через рекурсию
-    if (n == 0)
-        return 1;
-    return n * Factorial_Recursive(n - 1);
-}
-
-unsigned int Factorial_Direct(unsigned int n){ //вычисление факториала через цикл
-    int res = 1;
-    for (int i = 1; i <= n; i++) {
-        res = res * i;
-    }
-    return res;
-}
-
-double Direct_Calculation(int n, double x) { // прямое вычисление значения функции
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += pow(x, 2 * i + 1) / Factorial_Direct(2 * i + 1);
-    } 
-    return sum; 
-}
-
-double Recursive_Calculation(int n, double x) { // рекурсивное вычисление функции
-    if (n == 0) {
-        return x; // когда вычисляется первый элемент ряда
-    }
-    else {                                                          
-        return pow(x, 2 * n + 1) / Factorial_Recursive(2 * n + 1) + Recursive_Calculation(n - 1, x);
-        //pow(x, 2 * n + 1)  возвожу в необходимую степень
-        //Factorial_Recursive(2 * n + 1)  вычисляю факториал
-        //Recursive_Calculation(n - 1, x)  прибавляю предыдущее значение ряда
-    }
-}
+unsigned int Factorial_Recursive(unsigned int n);
+unsigned int Factorial_Direct(unsigned int n);
+double Direct_Calculation(int n, double x);
+double Recursive_Calculation(int n, double x);
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -76,4 +47,38 @@ int main() {
     else cout << endl << "Прямое вычисление значения функции быстрее";
 
     return 0;
+}
+
+unsigned int Factorial_Recursive(unsigned int n) { //вычисление факториала через рекурсию
+    if (n == 0)
+        return 1;
+    return n * Factorial_Recursive(n - 1);
+}
+
+unsigned int Factorial_Direct(unsigned int n) { //вычисление факториала через цикл
+    int res = 1;
+    for (int i = 1; i <= n; i++) {
+        res = res * i;
+    }
+    return res;
+}
+
+double Direct_Calculation(int n, double x) { // прямое вычисление значения функции
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += pow(x, 2 * i + 1) / Factorial_Direct(2 * i + 1);
+    }
+    return sum;
+}
+
+double Recursive_Calculation(int n, double x) { // рекурсивное вычисление функции
+    if (n == 0) {
+        return x; // когда вычисляется первый элемент ряда
+    }
+    else {
+        return pow(x, 2 * n + 1) / Factorial_Recursive(2 * n + 1) + Recursive_Calculation(n - 1, x);
+        //pow(x, 2 * n + 1)  возвожу в необходимую степень
+        //Factorial_Recursive(2 * n + 1)  вычисляю факториал
+        //Recursive_Calculation(n - 1, x)  прибавляю предыдущее значение ряда
+    }
 }
