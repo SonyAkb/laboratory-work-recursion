10e82b4cfe84e4209edd3eecd005b70f9adf5457
SonyAkb <145361459+SonyAkb@users.noreply.github.com>
2024-02-08 23:03:14 +0500

    Числа Фибоначчи

---
+++ Числа Фибоначчи.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int amount;
+    cout << "Введите количество чисел Фибоначчи, которое нужно вывести ";
+    cin >> amount;
+    while (amount < 1) {
+        cout << "Количество чисел не может быть меньше 1. Попробуйте еще раз: ";
+        cin >> amount;
+    }
+    for (int num = 0; num < amount; num++) {
+        cout << fibonacci(num) << endl;
+    }
+
+    return 0;
+}
